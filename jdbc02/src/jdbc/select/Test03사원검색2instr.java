3d80a0fc444cb48df36e4c134685dc6c714c6be2
kwonseozero <kwonseozero@naver.com>
2024-01-09 23:56:53 +0900

    사원검색 like, instr

---
+++ jdbc02/src/jdbc/select/Test03사원검색2instr.java
@@ -0,0 +1,30 @@
+package jdbc.select;
+
+import java.util.List;
+import java.util.Scanner;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import jdbc.uril.jdbcHelper;
+
+public class Test03사원검색2instr {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("사원의 이름을 입력하세요.");
+		String keyword = sc.nextLine();
+		
+		JdbcTemplate jdbcTemplate = jdbcHelper.getJdbcTemplate();
+		String sql = "select * from emp "
+				+ "where instr(emp_name, ?) >0";
+		Object[] data = {keyword};
+		EmpMapper mapper = new EmpMapper();
+	
+	List<EmpDto> list = jdbcTemplate.query(sql,mapper,data);
+	
+	System.out.println("조회 결과: " +list.size());
+	for(EmpDto dto : list) {
+		System.out.println(dto.getEmpName());
+	}
+	}
+}
