a5c72b4db8ab7bda60a748f320d492d254e02a58
kwonseozero <kwonseozero@naver.com>
2024-01-09 20:47:50 +0900

    사원정보 수정 문제 풀이

--- jdbc02/src/jdbc/update/Test01포켓몬수정.java
+++ jdbc02/src/jdbc/update/Test01포켓몬수정.java
@@ -1,7 +1,31 @@
 package jdbc.update;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import jdbc.uril.jdbcHelper;
+
 public class Test01포켓몬수정 {
 	public static void main(String[] args) {
+		//목표: 1개의 포켓몬스터 정보 중 이름과 유형을 변경하도록 구현
 		
+		//데이터 준비
+		int pocketmonNo = 1;
+		String pocketmonName="테스트";
+		String pocketmonType = "테스트";
+		
+		//도구를 한번에 생성
+		JdbcTemplate jdbcTemplate = jdbcHelper.getJdbcTemplate();//생성메소드
+	
+		String sql = "update pocketmon "
+						+ "set pocketmon_name = ?, pocketmon_type=? "
+						+ "where pocketmon_no=?";
+		
+		Object[] data = {pocketmonName, pocketmonType, pocketmonNo};
+	
+		jdbcTemplate.update(sql, data);
+		
+		//출력
+		System.out.println("정보 변경 완료");
 	}
 }
---
+++ jdbc02/src/jdbc/uril/jdbcHelper.java
@@ -0,0 +1,22 @@
+package jdbc.uril;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+public class jdbcHelper {
+	//JdbcTemplate을 생성하는 메소드 구현
+	//= 아무나 객체 없이 접근 가능하게 구현
+	public static JdbcTemplate getJdbcTemplate() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setUsername("kh13");
+		dataSource.setPassword("kh13");
+		dataSource.setDriverClassName("oracle.jdbc.OracleDriver");
+		dataSource.setUrl("jdbc:oracle:thin:@localhost:1521:xe");
+		
+		JdbcTemplate jdbcTemplate = new JdbcTemplate();
+		jdbcTemplate.setDataSource(dataSource);
+		
+		return jdbcTemplate;
+	}
+	
+}
