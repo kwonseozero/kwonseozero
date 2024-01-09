39b84e1c6049a53a553e497184ec6cec4a7847bc
kwonseozero <kwonseozero@naver.com>
2024-01-09 23:14:27 +0900

    메뉴 조회(+검색)

---
+++ jdbc02/src/jdbc/select/Test03메뉴목록2.java
@@ -0,0 +1,33 @@
+package jdbc.select;
+
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import jdbc.uril.jdbcHelper;
+
+public class Test03메뉴목록2 {
+	public static void main(String[] args) {
+		
+		String keyword = "카페라떼";
+		
+		JdbcTemplate jdbcTemplate = jdbcHelper.getJdbcTemplate();
+		
+		String sql = "select * from menu where menu_name_kor=? order by menu_no asc";
+		Object[] data = {keyword};
+		MenuMapper mapper = new MenuMapper();
+		
+		List<MenuDto> list = jdbcTemplate.query(sql, mapper, data); //데이터가 있을시
+//		List<MenuDto> list = jdbcTemplate.query(sql, mapper);
+	
+		for(MenuDto dto : list) {
+//			System.out.println(dto.toString());
+			System.out.print(dto.getMenuNameEng());
+			System.out.print("(");
+			System.out.print(dto.getMenuNameEng());
+			System.out.print(")");
+			System.out.print("-");
+			System.out.println(dto.getMenuPrice()+"원");
+				}
+	}
+}
