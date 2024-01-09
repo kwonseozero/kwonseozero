152063c2ab050325d7f0556aa99a99627ea76fc1
kwonseozero <kwonseozero@naver.com>
2024-01-09 21:25:08 +0900

    메뉴 정보 수정, 포켓몬 삭제

---
+++ jdbc02/src/jdbc/delete/Test01포켓몬삭제.java
@@ -0,0 +1,32 @@
+package jdbc.delete;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import jdbc.uril.jdbcHelper;
+
+public class Test01포켓몬삭제 {
+	public static void main(String[] args) {
+		
+		//포켓몬스터를 삭제하려면 "번호"만 알면 된다(번호가 기본키PK니까)
+		
+		//데이터 준비
+		int pocketmonNo = 1;
+		
+		//처리
+		JdbcTemplate jdbcTemplate = jdbcHelper.getJdbcTemplate();
+		
+		String sql = "delete pocketmon where pocketmon_no=?";
+		Object[] data = {pocketmonNo};
+		
+		int result = jdbcTemplate.update(sql, data);
+		
+		//출력
+		if(result > 0) {
+			System.out.println("포켓몬스터 삭제 완료");
+		}
+		else {
+			System.out.println("존재하지 않는 포켓몬스터 번호");
+		}
+		
+	}
+}
