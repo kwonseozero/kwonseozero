4804e5904cabd909133758b6c24e692f846e1577
kwonseozero <kwonseozero@naver.com>
2024-01-28 14:56:39 +0900

    과제 업로드

---
+++ spring11work/src/main/java/com/kh/spring11/Spring11workApplication.java
@@ -0,0 +1,13 @@
+package com.kh.spring11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring11workApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring11workApplication.class, args);
+	}
+
+}
